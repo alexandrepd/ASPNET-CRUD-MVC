a79b726b500f910d074052542899e084e2316e6b
Alexand Alves <alexandre_pd@hotmail.com>
2022-07-07 16:40:07 -0300

    adding solution

---
+++ ASPNET-CRUD-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ASPNET_CRUD_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
