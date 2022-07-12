eeeaa64b3782a0cae4cb4c7770df2001e67230d9
Alexand Alves <alexandre_pd@hotmail.com>
2022-07-12 09:12:55 -0300

    updating contact features

--- ASPNET-CRUD-MVC/Controllers/ContactController.cs
+++ ASPNET-CRUD-MVC/Controllers/ContactController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNET_CRUD_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,6 +16,27 @@ namespace ASPNET_CRUD_MVC.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult CreateContact()
+        {
+            return View();
+        }
+
+        public IActionResult EditContact()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmToDeleteContact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- ASPNET-CRUD-MVC/Controllers/HomeController.cs
+++ ASPNET-CRUD-MVC/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace ASPNET_CRUD_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
